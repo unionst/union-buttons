b3da07592437a1d4472afe72cbc1f68c5f19e42a
blsage <ben@unionst.com>
2025-11-17 19:11:50 -0500

    Require union-gestures 1.0.1+ for Swift 6.1 compatibility

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/unionst/union-haptics.git", .upToNextMajor(from: "1.2.1")),
-        .package(url: "https://github.com/unionst/union-gestures.git", .upToNextMajor(from: "1.0.0"))
+        .package(url: "https://github.com/unionst/union-gestures.git", .upToNextMajor(from: "1.0.1"))
     ],
     targets: [
         .target(
